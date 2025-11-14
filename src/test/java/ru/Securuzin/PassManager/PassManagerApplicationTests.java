dc380f86e4e3493e3e82d2c81785674484e4efbb
birthtree <vladimirakkuzin10@gmail.com>
2025-11-14 19:50:53 +0300

    Initial commit: Password Manager project setup

---
+++ src/test/java/ru/Securuzin/PassManager/PassManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Securuzin.PassManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
